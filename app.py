560244b2a7f13b61994b0a8163376c1e42b0cf61
Zeksait <kakoitov@gmail.com>
2021-11-06 15:36:35 +0300

    Add flask_sqlalchemy, init database

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request
+from models.database import db
 
 app = Flask(__name__)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI='"sqlite:///./seasons.db',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+db.init_app(app)
 
 
 @app.route("/")
