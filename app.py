ca03ec23dda925768b18dbc85919c7b3a9eeac44
Zeksait <kakoitov@gmail.com>
2021-11-24 21:45:20 +0300

    add flask_crontab

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, request, render_template
 from models.database import db
 from flask_migrate import Migrate
 from views.seasons import seasons_app
+from flask_crontab import Crontab
+import random
 
 app = Flask(__name__)
+crontab = Crontab(app)
 app.register_blueprint(seasons_app, url_prefix="/seasons")
 app.config.update(
     SQLALCHEMY_DATABASE_URI="sqlite:///./seasons.db",
@@ -11,6 +14,8 @@ app.config.update(
 )
 db.init_app(app)
 migrate = Migrate(app, db)
+app.debug = True
+data = []
 
 
 @app.route("/")
@@ -21,4 +26,10 @@ def hello_world():
 
 @app.route("/dashboard/")
 def hello_name():
-    return render_template("dashboard/index.html", content='Test')
+    return render_template("dashboard/index.html", data=data)
+
+
+@crontab.job(minute="1", hour="0")
+def my_scheduled_job():
+    data.append(random.randint(0, 10))
+    print(data)
--- static/js/dashboard/dashboard.js
+++ static/js/dashboard/dashboard.js
@@ -1,53 +1,54 @@
 /* globals Chart:false, feather:false */
 
 (function () {
-  'use strict'
+    'use strict'
 
-  feather.replace({ 'aria-hidden': 'true' })
+    feather.replace({'aria-hidden': 'true'})
 
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ],
-      datasets: [{
-        data: [
-          15339,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          12034
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
+    // Graphs
+    var ctx = document.getElementById('myChart')
+    // eslint-disable-next-line no-unused-vars
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [
+                'Sunday',
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday'
+            ],
+            datasets: [
+                {
+                    data: [15, 21, 18, 24, 23, 24, 12],
+                    lineTension: 0,
+                    backgroundColor: 'transparent',
+                    borderColor: '#007bff',
+                    borderWidth: 4,
+                    pointBackgroundColor: '#007bff'
+                },
+                {
+                    data: [1, 2, 3, 4, 2, 5, 7],
+                    lineTension: 0,
+                    backgroundColor: 'transparent',
+                    borderColor: '#ff0000',
+                    borderWidth: 4,
+                    pointBackgroundColor: '#800000'
+                }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }]
+            },
+            legend: {
+                display: true
+            }
+        }
+    })
 })()
